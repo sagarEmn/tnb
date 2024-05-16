9abf97226c911366f49f52fe0e2901c6530e3846
Sagar <100994834+sagarEmn@users.noreply.github.com>
2024-05-16 14:46:27 +0545

    contactUs.jsx re-corrected

--- src/components/Home/ContactUs/ContactUs.jsx
+++ src/components/Home/ContactUs/ContactUs.jsx
@@ -3,6 +3,7 @@ import styles from "../../../global.module.css";
 import { contactImageSources } from "../../globalImageSources";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Footer from "../../Footer/Footer";
 
 export default function ContactUs() {
   useEffect(() => {
@@ -21,9 +22,13 @@ export default function ContactUs() {
           className={styles["padding-group-middle-grayBackground"]}
           data-aos="fade-up"
         >
-          <div className={styles["title-description-image-container"]}>
+          <div className={styles["title-description-image-container-grid"]}>
             <div className={styles["title-description-flexGap"]}>
               <p className={styles["first-line"]}>Contact Us</p>
+              <p className={styles["third-line"]}>
+                "We don't want to push our ideas on to customers, <br /> we
+                simply want to make what they want."
+              </p>
 
               <div className={styles["contact-us-information"]}>
                 <div className={styles["contact-icons"]}>
@@ -31,10 +36,12 @@ export default function ContactUs() {
                 </div>
 
                 <div className={styles["contact-details"]}>
-                  <p>HQ DUBAI OFFICE:</p>
-                  <p>One RX Interior Decoration LLC Plot No - 598-709 </p>
-                  <p>Next to Dubai Metal Grating & Manufcturing LLC DIP - 1 </p>
-                  <p>Dubai / UAE </p>
+                  <p> บริษัท ทีเอ็นบี ซอร์ส จำกัด 20/1 (ห้องK19/2)</p>
+                  <p>
+                    {" "}
+                    ถนนพัฒนาชนบท 3 แขวงคลองสองต้นนุ่น เขตลาดกระบัง
+                    จังหวัดกรุงเทพมหานคร 10520 โทร.{" "}
+                  </p>
                 </div>
               </div>
 
@@ -44,29 +51,9 @@ export default function ContactUs() {
                 </div>
 
                 <div className={styles["contact-details"]}>
-                  <p>+971 4 590 4700</p>
-                </div>
-              </div>
-
-              <div className={styles["contact-us-information"]}>
-                <div className={styles["contact-icons"]}>
-                  <img src={contactImageSources[2]} alt="Map Icon" />
-                </div>
-
-                <div className={styles["contact-details"]}>
-                  <p>TURKEY OFFICE:</p>
-                  <p>Orta Mah. İbrahim Ağa Cad. No:18 Kısmet Han </p>
-                  <p>Zemin kat Bayrampaşa, İstanbul / TÜRKİYE </p>
-                </div>
-              </div>
-
-              <div className={styles["contact-us-information"]}>
-                <div className={styles["contact-icons"]}>
-                  <img src={contactImageSources[3]} alt="Phone Icon" />
-                </div>
-
-                <div className={styles["contact-details"]}>
-                  <p>+90 212 437 8072</p>
+                  <p> Tel 02-138-9530</p>
+                  <p> Mobile 09-4857-4030</p>
+                  <p> เลขประจำตัวผู้เสียภาษี 0 1 0 5 5 5 9 0 8 9 8 1 7</p>
                 </div>
               </div>
 
@@ -76,17 +63,19 @@ export default function ContactUs() {
                 </div>
 
                 <div className={styles["contact-details"]}>
-                  <p>info@one-rx.com</p>
+                  <p>tnbsource.th@gmail.com</p>
                 </div>
               </div>
 
               <div className={styles["contact-us-information"]}>
                 <div className={styles["contact-icons"]}>
-                  <img src={contactImageSources[5]} alt="Mail Icon" />
+                  <img src={contactImageSources[1]} alt="Phone Icon" />
                 </div>
 
                 <div className={styles["contact-details"]}>
-                  <p>infoturkey@one-rx.com</p>
+                  <p> +66 (0) 948 574 030</p>
+                  <p> 0 617 849 898</p>
+                  <p> 02-138-9530</p>
                 </div>
               </div>
             </div>
@@ -122,11 +111,6 @@ export default function ContactUs() {
                   ></textarea>
                 </div>
               </div>
-              <p className={styles["description-one"]}>
-                If you are interested in working with us then please dop us a
-                line, or contact us below informations, we would love to hear
-                from you.
-              </p>
             </div>
           </div>
         </div>
