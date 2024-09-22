cb01fb00ce9537f781f592e3aa0760380e15645f
Sagar <jussagar1@gmail.com>
2024-09-22 22:20:44 +0545

    Gallery minor changes

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-
-import globalStyles from "../../global.module.css";
 import galleryImageSources from "./GalleryImageSources";
 
 function Gallery() {
@@ -15,55 +13,34 @@ function Gallery() {
   }, []);
 
   return (
-    <>
-      {/* nav location */}
-      <div
-        className={globalStyles["small-grayBackground"]}
-        data-aos="zoom-in"
-        data-aos-offset="0"
-      >
-        <div className={globalStyles["display-nav-location-center"]}>
-          <div className={globalStyles["first-line"]}>GALLERY</div>
-          <div className={globalStyles["px14-line"]}>HOME {">"} GALLERY</div>
+    <div className="bg-gray-100 min-h-screen">
+      <div className="bg-gray-200 py-8" data-aos="zoom-in" data-aos-offset="0">
+        <div className="container mx-auto text-center">
+          <h1 className="text-3xl font-bold mb-2">GALLERY</h1>
+          <p className="text-sm text-gray-600">HOME &gt; GALLERY</p>
         </div>
       </div>
-      <div className="container mx-auto px-2">
-        <div className={globalStyles["padding-container"]}>
-          <div className={globalStyles.galleryRow} data-aos="zoom-in">
-            <img
-              src={galleryImageSources[0]}
-              border="0"
-              className={globalStyles.gallerySectionImage}
-            />
-            <img
-              src={galleryImageSources[1]}
-              border="0"
-              className={globalStyles.gallerySectionImage}
-            />
-          </div>
-          <div className={globalStyles.galleryRow} data-aos="zoom-in">
-            <img
-              src={galleryImageSources[2]}
-              border="0"
-              className={globalStyles.gallerySectionImage}
-            />
-
-            <img
-              src={galleryImageSources[3]}
-              border="0"
-              className={globalStyles.gallerySectionImage}
-            />
-          </div>
-          <div className={globalStyles.galleryRowCenter} data-aos="zoom-in">
-            <img
-              src={galleryImageSources[4]}
-              border="0"
-              className={globalStyles.gallerySectionImageCenter}
-            />
-          </div>
+      <div className="container mx-auto px-1 lg:px-4 py-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {galleryImageSources.slice(0, 4).map((src, index) => (
+            <div key={index} className="w-full" data-aos="zoom-in">
+              <img
+                src={src}
+                alt={`Gallery image ${index + 1}`}
+                className="w-full h-64 object-cover"
+              />
+            </div>
+          ))}
+        </div>
+        <div className="mt-4" data-aos="zoom-in">
+          <img
+            src={galleryImageSources[4]}
+            alt="Gallery image 5"
+            className="w-full h-64 object-contain"
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
