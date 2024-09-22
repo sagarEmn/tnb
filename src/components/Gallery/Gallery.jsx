32b3c1a769c4d3302fc3133d4d38a25d342db433
Sagar <jussagar1@gmail.com>
2024-09-22 22:16:42 +0545

    Minor changes

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -16,60 +16,45 @@ function Gallery() {
 
   return (
     <>
+      {/* nav location */}
+      <div
+        className={globalStyles["small-grayBackground"]}
+        data-aos="zoom-in"
+        data-aos-offset="0"
+      >
+        <div className={globalStyles["display-nav-location-center"]}>
+          <div className={globalStyles["first-line"]}>GALLERY</div>
+          <div className={globalStyles["px14-line"]}>HOME {">"} GALLERY</div>
+        </div>
+      </div>
       <div className="container mx-auto px-2">
         <div className={globalStyles["padding-container"]}>
-          {/* nav location */}
-          <div
-            className={globalStyles["small-grayBackground"]}
-            data-aos="zoom-in"
-            data-aos-offset="0"
-          >
-            <div className={globalStyles["display-nav-location-center"]}>
-              <div className={globalStyles["first-line"]}>GALLERY</div>
-              <div className={globalStyles["px14-line"]}>
-                HOME {">"} GALLERY
-              </div>
-            </div>
-          </div>
-
-          <div
-            className={globalStyles.galleryRow}
-            data-aos="zoom-in"
-          >
+          <div className={globalStyles.galleryRow} data-aos="zoom-in">
             <img
               src={galleryImageSources[0]}
               border="0"
               className={globalStyles.gallerySectionImage}
             />
-             <img
+            <img
               src={galleryImageSources[1]}
               border="0"
               className={globalStyles.gallerySectionImage}
             />
           </div>
-          <div
-            className={globalStyles.galleryRow}
-            data-aos="zoom-in"
-          >
-           
-         
-       
+          <div className={globalStyles.galleryRow} data-aos="zoom-in">
             <img
               src={galleryImageSources[2]}
               border="0"
               className={globalStyles.gallerySectionImage}
             />
-      
+
             <img
               src={galleryImageSources[3]}
               border="0"
               className={globalStyles.gallerySectionImage}
             />
           </div>
-          <div
-            className={globalStyles.galleryRowCenter}
-            data-aos="zoom-in"
-          >
+          <div className={globalStyles.galleryRowCenter} data-aos="zoom-in">
             <img
               src={galleryImageSources[4]}
               border="0"
